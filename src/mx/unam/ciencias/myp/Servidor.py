2c343f0e703b89282732c575052913cca8363f7f
Eric <giovanny.m.t@ciencias.unam.mx>
2018-09-13 23:44:54 -0500

    Servidor funcionando (Sin protocolo)

--- src/mx/unam/ciencias/myp/Servidor.py
+++ src/mx/unam/ciencias/myp/Servidor.py
@@ -22,6 +22,58 @@ class Servidor:
         self.sock.close()
         self.sock = sock
 
+    def initServer(self):
+        self.initDaemon()
+        self.recibirMensaje()
+
+    def recibirMensaje(self):
+        while True:
+            msg = input()
+            if msg == "QUIT":
+                self.sock.close()
+                sys.exit()
+            else:
+                pass
+
+    def initDaemon(self):
+        aceptar = threading.Thread(target=self.aceptarConexion)
+        procesar = threading.Thread(target=self.esperarMensaje)
+        aceptar.daemon = True
+        aceptar.start()
+        procesar.daemon = True
+        procesar.start()
+    
+    def msgToAll(self, msg, cliente):
+        for c in self.clientes:
+            try:
+                if c != cliente:
+                    c.send(msg)
+            except:
+                self.clientes.remove(c)
+    
+    def aceptarConexion(self):
+        print("Esperando conexiones...")
+        while True:
+            try:
+                conn, addr = self.sock.accept()
+                conn.setblocking(False)
+                print(addr)
+                self.clientes.append(conn)
+            except:
+                pass
+
+    def esperarMensaje(self):
+        print("Esperando Mensajes...")
+        while True:
+            if len(self.clientes) > 0:
+                for c in self.clientes:
+                    try:
+                        data = c.recv(1024)
+                        if data:
+                            self.msgToAll(data,c)
+                    except:
+                        pass
+
 if __name__ == "__main__":
     try:
         s = Servidor(str(sys.argv[1]), int(sys.argv[2]))
