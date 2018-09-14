76374cadf11f35ea0d55b96f2aa94b438101ccf1
Eric <giovanny.m.t@ciencias.unam.mx>
2018-09-13 23:20:37 -0500

    Test Cliente: Test get user address

--- src/mx/unam/ciencias/myp/TestCliente.py
+++ src/mx/unam/ciencias/myp/TestCliente.py
@@ -12,6 +12,12 @@ class TestCliente(unittest.TestCase):
         self.cliente.setUserAddress(("localhost",1))
         self.assertEqual(("localhost",1), self.cliente.getUserAddress())
         self.cliente.cerrarSocket()
+
+    def testGetUserAddress(self):
+        self.cliente = Cliente.Cliente("localhost", 2)
+        self.cliente.setUserAddress(("localhost",2))
+        self.assertEqual(("localhost",2), self.cliente.getUserAddress())
+        self.cliente.cerrarSocket()
     
 if __name__ == "__main__":
     unittest.main()
