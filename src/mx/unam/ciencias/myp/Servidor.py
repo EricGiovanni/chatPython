d70b0ec478a3b90478cd7f7755f276f59b561b1b
Eric <giovanny.m.t@ciencias.unam.mx>
2018-09-19 23:18:23 -0500

    Comenzando la interfaz

--- src/mx/unam/ciencias/myp/Servidor.py
+++ src/mx/unam/ciencias/myp/Servidor.py
@@ -2,20 +2,27 @@ import socket
 import threading
 import sys
 import pickle
+import Room
 
 class Servidor:
     def __init__(self, host, port):
         self.clientes = []
+        self.rooms = []
+        self.principal = Room.Room(self.clientes, "Principal")
+        self.rooms.append(self.principal)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = (str(host), int(port))
-        self.numUser = 0
         self.id = 0
+        self.numUser = "User" + str(self.id)
 
     def aumentaID(self):
         self.id = self.id + 1
 
     def aumentaNumUser(self):
-        self.numUser = self.numUser + 1
+        self.numUser = "User" + str(self.id)
+
+    def setNumUser(self, numUser):
+        self.numUser = numUser
 
     def getID(self):
         return self.id
@@ -63,6 +70,9 @@ class Servidor:
         procesar.daemon = True
         procesar.start()
     
+    #def mstToAll(self, msg, cliente):
+        #if
+
     def msgToAll(self, msg, cliente):
         for c in self.clientes:
             try:
@@ -74,10 +84,15 @@ class Servidor:
     def privateMsg(self, msg, cliente):
         for c in self.clientes:
             try:
-                if c[0] in cliente:
+                if c[0] == cliente:
                     c[0].send(msg)
             except:
                 self.clientes.remove(c)
+    
+    def groupMsg(self, nombre, msg, cliente):
+        for r in self.rooms:
+            if r.getNombre() == nombre:
+                r.enviarMsg(msg, cliente)
     
     def aceptarConexion(self):
         print("Esperando conexiones...")
@@ -85,10 +100,13 @@ class Servidor:
             try:
                 conn, addr = self.sock.accept()
                 conn.setblocking(False)
-                print(addr)
+                print("Se ha conectado" + str(addr))
                 self.clientes.append((conn, self.numUser, self.id))
-                self.aumentaNumUser()
+                for r in self.rooms:
+                    if r.getNombre == "Principal":
+                        r.agregarCliente((conn, self.numUser, self.id))
                 self.aumentaID()
+                self.aumentaNumUser()
             except:
                 pass
 
@@ -102,17 +120,37 @@ class Servidor:
                         if data:
                             if(pickle.loads(data) == "DISCONNECT"):
                                 msg = "Se ha desconectado " + str(c[1])
+                                print(msg)
                                 msgBytes = pickle.dumps(msg)
-                                print(msgBytes)
                                 self.msgToAll(msgBytes, c[0])
+                            elif(pickle.loads(data).find("STATUS", 0, 6) != -1):
+                                msg = pickle.loads(data)
+                                if msg.find("AWAY", 7, 11) != -1:
+                                    msg = pickle.dumps(str(c[1]) + " ha cambiado su status a AWAY")
+                                    self.msgToAll(msg, c[0])
+                                elif msg.find("BUSY", 7, 11) != -1:
+                                    msg = pickle.dumps(str(c[1]) + " ha cambiado su status a BUSY")
+                                    self.msgToAll(msg, c[0])
+                                elif msg.find("ACTIVE", 7, 13) != -1:
+                                    msg = pickle.dumps(str(c[1]) + " ha cambiado su status a ACTIVE")
+                                    self.msgToAll(msg, c[0])
+                                else:
+                                    msg = pickle.dumps(msg)
+                                    self.msgToAll(msg, c[0])
+                            elif(pickle.loads(data).find("PUBLICMESSAGE", 0, 13) != -1):
+                                msg = str(c[1]) + ": " + pickle.loads(data)[14:]
+                                self.msgToAll(pickle.dumps(msg), c[0])
+                            elif(pickle.loads(data).find("IDENTIFY", 0, 8) != -1):
+                                #c[1] = str(pickle.loads(data)[9:])
+                                self.privateMsg(pickle.dumps("Tu nombre ha cambiado a: " + str(c[1])), c[0])
                             else:
-                                self.msgToAll(data,c[0])
+                                self.privateMsg(pickle.dumps("Comando incorrecto " + str(c[1])), c[0])
                     except:
                         pass
 
 if __name__ == "__main__":
     try:
-        s = Servidor(str(sys.argv[1]), int(sys.argv[2]))
+        s = Servidor("0.0.0.0", int(sys.argv[1]))
         s.initServer()
     except:
         print("use: python3 servidor.py host port")
