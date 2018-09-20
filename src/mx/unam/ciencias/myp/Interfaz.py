918e119431178d1531cd0a8c6231e9d307c8148d
Eric <giovanny.m.t@ciencias.unam.mx>
2018-09-20 12:32:04 -0500

    Cambios a interfaz

--- src/mx/unam/ciencias/myp/Interfaz.py
+++ src/mx/unam/ciencias/myp/Interfaz.py
@@ -1,21 +1,21 @@
+import Cliente
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-class MyWindow(Gtk.Window):
+class Interfaz(Gtk.Window):
 
     def __init__(self):
-        Gtk.Window.__init__(self, title="Hello World")
-
-        self.button = Gtk.Button(label="Click Here")
-        self.button.connect("clicked", self.on_button_clicked)
+        Gtk.Window.__init__(self, title="Chat")
+        self.button = Gtk.Button(label="Enviar mensaje")
+        self.button.connect("Enviado", self.on_button_clicked)
         self.add(self.button)
 
     def on_button_clicked(self, widget):
         print("Hello World")
 
 if __name__ == "__main__":
-    win = MyWindow()
+    win = Interfaz()
     win.connect("destroy", Gtk.main_quit)
     win.show_all()
     Gtk.main()
